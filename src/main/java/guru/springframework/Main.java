233c00996f7320b93a3702ca851eb518a832fb8c
Donald F. Coffin <donald.coffin@reminetworks.com>
2024-04-21 10:43:15 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/Main.java
@@ -0,0 +1,10 @@
+package guru.springframework;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ **/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
