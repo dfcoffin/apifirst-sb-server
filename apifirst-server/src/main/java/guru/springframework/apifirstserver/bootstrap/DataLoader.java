7cae0e95267b5f6151fe97b45cec254fa3f84771
Donald F. Coffin <donald.coffin@reminetworks.com>
2024-04-22 04:09:23 -0400

    Initialize data for testing

---
+++ apifirst-server/src/main/java/guru/springframework/apifirstserver/bootstrap/DataLoader.java
@@ -0,0 +1,75 @@
+package guru.springframework.apifirstserver.bootstrap;
+
+import com.reminetworks.apifirst.model.Address;
+import com.reminetworks.apifirst.model.Customer;
+import com.reminetworks.apifirst.model.Name;
+import com.reminetworks.apifirst.model.PaymentMethod;
+import guru.springframework.apifirstserver.repositories.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ **/
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements CommandLineRunner {
+
+	private final CustomerRepository customerRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Address address1 = Address.builder()
+				.addressLine1("1234 W Some Street")
+				.city("Some City")
+				.state("FL")
+				.zip("33701")
+				.build();
+
+		Customer customer1 = Customer.builder()
+				.name(Name.builder()
+						.firstName("John")
+						.lastName("Thompson")
+						.build())
+				.billToAddress(address1)
+				.shipToAddress(address1)
+				.email("john@springframework.guru")
+				.phone("800-555-1212")
+				.paymentMethods(List.of(PaymentMethod.builder()
+								.cardNumber(12341234)
+								.expiryMonth(12)
+								.expiryYear(26)
+						.build()))
+				.build();
+
+		Address address2 = Address.builder()
+				.addressLine1("1234 W Some Street")
+				.city("Some City")
+				.state("FL")
+				.zip("33701")
+				.build();
+
+		Customer customer2 = Customer.builder()
+				.name(Name.builder()
+						.firstName("Jim")
+						.lastName("Smith")
+						.build())
+				.billToAddress(address1)
+				.shipToAddress(address1)
+				.email("jim@springframework.guru")
+				.phone("800-555-1212")
+				.paymentMethods(List.of(PaymentMethod.builder()
+						.cardNumber(1234888)
+						.expiryMonth(12)
+						.expiryYear(26)
+						.build()))
+				.build();
+
+		customerRepository.save(customer1);
+		customerRepository.save(customer2);
+	}
+}
--- apifirst-server/src/test/java/guru/springframework/apifirstserver/ApifirstServerApplicationTests.java
+++ apifirst-server/src/test/java/guru/springframework/apifirstserver/ApifirstServerApplicationTests.java
@@ -1,13 +1,25 @@
 package guru.springframework.apifirstserver;
 
+import guru.springframework.apifirstserver.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class ApifirstServerApplicationTests {
 
+	@Autowired
+	CustomerRepository customerRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testDataLoad() {
+		assertThat(customerRepository.count()).isGreaterThan(0L);
 	}
 
 }
