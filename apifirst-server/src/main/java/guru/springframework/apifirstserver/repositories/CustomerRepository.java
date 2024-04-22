23eae577942571be510a595587aeead29ad4f261
Donald F. Coffin <donald.coffin@reminetworks.com>
2024-04-22 03:15:59 -0400

    Adding customer repo

---
+++ apifirst-server/src/main/java/guru/springframework/apifirstserver/repositories/CustomerRepository.java
@@ -0,0 +1,12 @@
+package guru.springframework.apifirstserver.repositories;
+
+import com.reminetworks.apifirst.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ **/
+public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+}
